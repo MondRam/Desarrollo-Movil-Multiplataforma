c5eed214e0c578d3490d936d8d1936372fb81a73
MondRam <utm22030544@utma.edu.mx>
2024-05-20 09:13:41 -0600

    feat: changes made

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,5 +1,32 @@
+import React, { useState } from "react";
 
 function Index() {
+  const [isEditting, setIsEditting] = useState(false);
+
+  const [user, setUser] = useState({
+    name: "Montserrat Guadalupe Ramírez",
+    carrera: "TIDSM",
+    grado: "6to",
+    num: "UTM22030544",
+  });
+
+ 
+  function handleSubmit(event) {
+    event.preventDefault();
+    setUser({
+      name: event.target.name.value,
+      carrera: event.target.carrera.value,
+      grado: event.target.grado.value,
+      num: event.target.num.value,
+    });
+
+    setIsEditting(false);
+  }
+
+  const handleEdit = () => {
+    setIsEditting(true);
+  };
+
   return (
     <div className="Index">
       <div class="inner-container">
@@ -7,34 +34,38 @@ function Index() {
           <img class="user" src="usuario.jpg" alt="user" />
         </div>
         <div class="right-container">
+          {isEditting ? (
+            <form onSubmit={handleSubmit} >
+              <label id="new-info" htmlFor="name">Nombre: &nbsp;</label>
+              <input id="name" alt="Name" type="text" defaultValue={user.name} />
+              <br />
 
-        <label id="new-info">Nombre: &nbsp;</label>
-        <input id="name" alt="Name" type="text" />
-        <br />
+              <label id="new-info" htmlFor="carrera">Carrera: &nbsp;</label>
+              <input id="carrera" alt="Carrera" type="text" defaultValue={user.carrera}/>
+              <br />
 
-        <label id="new-info">Carrera: &nbsp;</label>
-        <input id="carrera" alt="Carrera" type="text" />
-        <br />
+              <label id="new-info" htmlFor="grado">Grado: &nbsp;</label>
+              <input id="grado" alt="Grado" type="text" defaultValue={user.grado}/>
+              <br />
 
-        <label id="new-info">Grado: &nbsp;</label>
-        <input id="grado" alt="Grado" type="text" />
-        <br />
+              <label id="new-info" htmlFor="num">No. de Control: &nbsp;</label>
+              <input id="num" alt="Numero" type="text" defaultValue={user.num}/>
+              <br />
 
-        <label id="new-info">No. de Control: &nbsp;</label>
-        <input id="num" alt="Numero" type="text" />
-        <br />
-
-        <button type="submit">Aceptar</button>
-
-          <p class="t">Nombre: Montserrat Guadalupe Ramírez</p>
-          <p class="t">Carrera:TIDSM </p>
-          <p class="t">Nivel: 6to</p>
-          <p class="t">No. de Control: UTM22030544</p>
-          <button type="change">Editar</button>
+              <button type="submit">Aceptar</button>
+            </form>
+          ) : (
+            <>
+              <p class="t">Nombre: {user.name}</p>
+              <p class="t">Carrera: {user.carrera} </p>
+              <p class="t">Grado: {user.grado}</p>
+              <p class="t">No. de Control: {user.num}</p>
+              <button onClick={handleEdit}>Editar</button>
+            </>
+          )}
         </div>
       </div>
     </div>
-    
   );
 }
 
