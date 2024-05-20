41f7cb754ef7dd3f498efb18038b14bc5ac7b96c
MondRam <utm22030544@utma.edu.mx>
2024-05-19 22:27:10 -0600

    feat: changes made

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -8,18 +8,28 @@ function Index() {
         </div>
         <div class="right-container">
 
+        <label id="new-info">Nombre: &nbsp;</label>
         <input id="name" alt="Name" type="text" />
-        <button>
-          <i class="fa fa-search" style="font-size: 18px;">
-          </i>
-        </button>
+        <br />
 
+        <label id="new-info">Carrera: &nbsp;</label>
+        <input id="carrera" alt="Carrera" type="text" />
+        <br />
 
+        <label id="new-info">Grado: &nbsp;</label>
+        <input id="grado" alt="Grado" type="text" />
+        <br />
 
-          {/* <p class="t">Nombre: Montserrat Guadalupe Ramírez</p>
+        <label id="new-info">Numero de Control: &nbsp;</label>
+        <input id="num" alt="Numero" type="text" />
+        <br />
+
+        <button type="submit">Enviar!</button>
+
+          <p class="t">Nombre: Montserrat Guadalupe Ramírez</p>
           <p class="t">Carrera:TIDSM </p>
           <p class="t">Nivel: 6to</p>
-          <p class="t">Numero de Control: UTM22030544</p> */}
+          <p class="t">Numero de Control: UTM22030544</p>
         </div>
       </div>
     </div>
